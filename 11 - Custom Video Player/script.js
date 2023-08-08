a698c6beed922c591a56a6890e9a6a4b01eeb3ab
harshit01010 <singhharshit01010@gmail.com>
2023-08-08 23:33:44 +0530

    key seq detection done

--- 11 - Custom Video Player/script.js
+++ 11 - Custom Video Player/script.js
@@ -3,6 +3,7 @@ const video = document.querySelector('.viewer')
 const progress = player.querySelector('.progress')
 const progressBar = player.querySelector('.progress__filled')
 const toggle = player.querySelector('.toggle')
+const toggleScreen = player.querySelector('.toggle_screen')
 const skipBtns = player.querySelectorAll('[data-skip')
 const ranges = player.querySelectorAll('.player__slider')
 
@@ -17,6 +18,19 @@ function updateBtn(){
     toggle.textContent = icon
 }
 
+function handleToggleScreen(){
+    if(player.classList.contains("fullscreen"))
+    {
+        player.classList.remove('fullscreen')
+        toggleScreen.innerHTML =' &#x26F6;';
+    }
+    else
+    {
+        player.classList.add("fullscreen")
+        toggleScreen.innerHTML =' &#x2BCF';
+    }
+}
+
 function skip(e){
     const val = this.dataset.skip
     video.currentTime += parseFloat(val)
@@ -26,7 +40,6 @@ function handleRangeUpdate(){
     const videoProperty = this.name
     video[videoProperty] = this.value
 }
-
 
 function handleProgress(){
     const percent = (video.currentTime / video.duration) * 100
@@ -43,6 +56,7 @@ video.addEventListener('play',updateBtn)
 video.addEventListener('pause',updateBtn)
 video.addEventListener('timeupdate',handleProgress)
 toggle.addEventListener('click',togglePlay)
+toggleScreen.addEventListener('click',handleToggleScreen)
 skipBtns.forEach(btn => btn.addEventListener('click',skip))
 ranges.forEach(range => range.addEventListener('change',handleRangeUpdate))
 ranges.forEach(range => range.addEventListener('mousemove',handleRangeUpdate))
@@ -52,3 +66,6 @@ let mousedown = false
 progress.addEventListener('mousemove',(e) => mousedown && moveToTime(e))
 progress.addEventListener('mousedown',() => mousedown = true)
 progress.addEventListener('mouseup',() => mousedown = false)
+
+window.addEventListener('keydown', (e) => (e.code === "KeyF") && handleToggleScreen())
+window.addEventListener('keydown', (e) => (e.code === "Space") && togglePlay())
